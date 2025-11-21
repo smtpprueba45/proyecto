b55383b5c9a3fc50b046d616f7d77fb6eb17e671
Usu2 Name <usu2@example.com>
2025-11-21 20:37:54 +0100

    Commit cambio2 en proyecto

---
+++ prueba1/src/cambio1/Cambio2.java
@@ -0,0 +1,10 @@
+package cambio1;
+
+public class Cambio2 {
+
+	public static void main(String[] args) {
+		System.out.println("Clase Cambio2 en proyecto");
+		System.out.println("Clase Cambio2 en proyecto");
+	}
+
+}
